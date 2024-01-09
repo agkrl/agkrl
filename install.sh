a2129cf63d955738f1b5ca36649bceabc666826a
Kirill <mikox67798@astegol.com>
2024-01-09 18:28:26 +0300

    test

--- install.sh
+++ install.sh
@@ -1,6 +1,4 @@
 sudo apt-get update
 sudo apt-get upgrade -y
-sudo apt-get install curl ruby-full bundler -y
 sudo curl -fsSL https://deb.nodesource.com/setup_20.x | sudo -E bash - &&\
 sudo apt-get install -y nodejs
-sudo npm i -g gulp
